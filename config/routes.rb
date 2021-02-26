86714b51e5b47a60a0ef4c98e958eb5b54b99460
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-02-26 12:24:50 +0100

    cleaning the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  # get 'doses/new'
+  # get 'doses/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :create, :show]
+  # get '/cocktails/:id/', to:'cocktails#show', as:'doses'
+  root to:"cocktails#index"
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
 end
